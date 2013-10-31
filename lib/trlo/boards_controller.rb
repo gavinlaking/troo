a488043021ae0b83a22214fa38eb32474a48c8f6
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-10-31 22:38:59 +0000

    Add BoardsController.

---
+++ lib/trlo/boards_controller.rb
@@ -0,0 +1,30 @@
+module Trlo
+  class BoardsController
+    def initialize(options, arguments)
+      @options   = options
+      @arguments = arguments
+    end
+
+    def self.dispatch!(options, arguments)
+      new(options, arguments).dispatch
+    end
+
+    def dispatch
+      options.each do |option|
+        case option
+        when :all
+          BoardPresenter.all
+        when :show
+          puts "Not implemented yet."
+        end
+      end
+    end
+
+    private
+    attr_reader :arguments
+
+    def options
+      @options.delete_if { |_, v| v.nil? }.keys
+    end
+  end
+end
---
+++ test/lib/trlo/test_boards_controller.rb
@@ -0,0 +1,41 @@
+require_relative "../../test_helper.rb"
+
+module Trlo
+  describe BoardsController do
+    describe "#dispatch" do
+      let(:arguments) {}
+
+      subject { BoardsController.new(options, arguments).dispatch }
+
+      describe ":all" do
+        let(:options)    { { all: true, show: nil, lists: nil } }
+        let(:board_1)    { OpenStruct.new(id: "f1e1e1d1", name: "Board 1") }
+        let(:board_2)    { OpenStruct.new(id: "f2e2e2d2", name: "Board 2") }
+        let(:board_hash) { [board_1, board_2] }
+
+        before do
+          Trello::Board.stubs(:all).returns(board_hash)
+        end
+
+        it "shows all the boards for the configured user" do
+          proc { subject }.must_output <<-TABLE.gsub(/^ {12}/, "")
+            +----------+---------+
+            | ID       | Name    |
+            +----------+---------+
+            | f1e1e1d1 | Board 1 |
+            | f2e2e2d2 | Board 2 |
+            +----------+---------+
+          TABLE
+        end
+      end
+
+      describe ":show" do
+        let(:options) { { all: nil, show: true, lists: nil } }
+
+        it "shows all the lists and cards for a <board_id>" do
+          proc { subject }.must_output "Not implemented yet.\n"
+        end
+      end
+    end
+  end
+end
