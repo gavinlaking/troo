439e653031d8bd677f84ace9e35a76849dcff57b
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-10-31 22:39:17 +0000

    Add ListsController.

---
+++ lib/trlo/lists_controller.rb
@@ -0,0 +1,31 @@
+module Trlo
+  class ListsController
+    def initialize(options, arguments)
+      @options   = options
+      @arguments = arguments
+    end
+
+    def self.dispatch!(options, arguments)
+      new(options, arguments).dispatch
+    end
+
+    def dispatch
+      options.each do |option|
+        case option
+        when :all
+          # puts "Not implemented yet."
+          board_id = arguments.first
+          ListPresenter.all(board_id)
+        end
+      end
+    end
+
+    private
+    attr_reader :arguments
+
+    def options
+      @options.delete_if { |_, v| v.nil? }.keys
+    end
+  end
+end
+
