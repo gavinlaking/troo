9bc21c529d50311d9be6bd6e7518de27640cf25d
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-11-01 21:38:02 +0000

    Add test file for CreateComment.

---
+++ test/lib/trlo/actions/test_create_comment.rb
@@ -0,0 +1,13 @@
+require_relative "../../test_helper"
+
+module Trlo
+  describe CreateComment do
+    describe ".for" do
+      subject { CreateComment.for(card_id) }
+
+      it "creates a comment on the card" do
+      	skip("Please write spec.")
+      end
+    end
+  end
+end
