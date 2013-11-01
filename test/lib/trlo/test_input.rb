6b93a67b24cba92f2c8326c0e8e9b5eab295ad48
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-11-01 21:38:32 +0000

    Add test file for Input.

---
+++ test/lib/trlo/test_input.rb
@@ -0,0 +1,13 @@
+require_relative "../../test_helper"
+
+module Trlo
+  describe Input do
+    describe ".get" do
+      subject { Input.get(id) }
+
+      it "gets user's input via $EDITOR" do
+        skip("Please write spec.")
+      end
+    end
+  end
+end
