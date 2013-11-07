6a6e5edbf9676ebbb4d0ce3ff09b553b3cc497d7
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-11-07 19:11:16 +0000

    Remove decorators and CreateComment

--- lib/trlo.rb
+++ lib/trlo.rb
@@ -13,18 +13,15 @@ require_relative "trlo/input"
 require_relative "trlo/screen"
 require_relative "trlo/version"
 
-require_relative "trlo/board_decorator"
 require_relative "trlo/board_presenter"
 require_relative "trlo/boards_controller"
 
-require_relative "trlo/list_decorator"
 require_relative "trlo/list_presenter"
 require_relative "trlo/lists_controller"
 
-require_relative "trlo/card_decorator"
 require_relative "trlo/card_presenter"
 require_relative "trlo/cards_controller"
-require_relative "trlo/actions/create_comment"
+
 require_relative "trlo/actions/move_card"
 
 module Trlo
