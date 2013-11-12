a4ed9d7704ce9d24ea8789832a0bf2f8c3796462
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-11-12 18:42:07 +0000

    Remove custom exceptions.

--- lib/trlo.rb
+++ lib/trlo.rb
@@ -8,7 +8,6 @@ require "trello"
 require "yaml"
 
 require_relative "trlo/configuration"
-require_relative "trlo/exceptions"
 require_relative "trlo/input"
 require_relative "trlo/output"
 require_relative "trlo/version"
--- lib/trlo/exceptions.rb
+++
@@ -1,6 +0,0 @@
-module Trlo
-  class BoardNotFound < StandardError; end
-  class ListNotFound < StandardError; end
-  class CardNotFound < StandardError; end
-end
-
