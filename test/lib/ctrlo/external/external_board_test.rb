bdb96982b5905fae57fcb576ef907a4f9bdb4389
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-12-04 23:04:43 +0000

    Remove commented out tests because Git.

--- test/lib/ctrlo/external/external_board_test.rb
+++ test/lib/ctrlo/external/external_board_test.rb
@@ -2,22 +2,5 @@ require_relative "../../../test_helper"
 
 module Ctrlo
   describe ExternalBoard do
-    # describe "when the external_board_id is provided" do
-    #   before { VCR.insert_cassette(:board_singular) }
-    #   after  { VCR.eject_cassette }
-
-    #   it "returns an individual board" do
-    #     Ctrlo::ExternalBoard.request("526d8e130a14a9d846001d96").size must_equal 1
-    #   end
-    # end
-
-    # describe "when the external_board_id is not provided" do
-    #   before { VCR.insert_cassette(:board_collection) }
-    #   after  { VCR.eject_cassette }
-
-    #   it "returns a collection of boards" do
-    #     Ctrlo::ExternalBoard.request.size must_equal 2
-    #   end
-    # end
   end
 end
--- test/lib/ctrlo/external/external_comment_test.rb
+++ test/lib/ctrlo/external/external_comment_test.rb
@@ -2,22 +2,5 @@ require_relative "../../../test_helper"
 
 module Ctrlo
   describe ExternalComment do
-    # describe "when the is_comment_id option is set" do
-    #   before { VCR.insert_cassette(:comment_singular) }
-    #   after  { VCR.eject_cassette }
-
-    #   it "returns an individual comment" do
-    #     Ctrlo::ExternalComment.request("526d8f19ddb279532e005259", {is_comment_id: true}).size must_equal 1
-    #   end
-    # end
-
-    # describe "when the is_comment_id option is not set" do
-    #   before { VCR.insert_cassette(:comment_collection) }
-    #   after  { VCR.eject_cassette }
-
-    #   it "returns a collection of comments" do
-    #     Ctrlo::ExternalComment.request("526d8e130a14a9d846001d96").size must_equal 17
-    #   end
-    # end
   end
 end
