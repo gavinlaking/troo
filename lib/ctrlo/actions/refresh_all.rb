c17042905897e25226890bd72439da43e8da4e6b
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-12-05 22:14:12 +0000

    Only bother to refresh boards that are not closed.

--- lib/ctrlo/actions/refresh_all.rb
+++ lib/ctrlo/actions/refresh_all.rb
@@ -1,13 +1,34 @@
 module Ctrlo
   class RefreshAll
+    def initialize
+    end
+
     def self.perform
-      RefreshBoards.all.map(&:external_board_id).map do |board_id|
-        RefreshLists.for     board_id
-        RefreshCards.for     board_id
-        RefreshComments.for  board_id
-        RefreshMembers.for   board_id
+      new.perform
+    end
+
+    def perform
+      external_board_ids.map do |id|
+        RefreshLists.for         id
+        RefreshCards.for         id
+        RefreshComments.for      id
+        RefreshMembers.for       id
       end
       true
+    end
+
+    private
+
+    def external_board_ids
+      active_boards.map(&:external_board_id)
+    end
+
+    def active_boards
+      all_boards.delete_if { |b| b.closed == true }
+    end
+
+    def all_boards
+      @boards ||= RefreshBoards.all
     end
   end
 end
