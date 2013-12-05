70d83151f7e81ccf72626e2f60f5ad4679f1b9f1
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-12-05 19:11:42 +0000

    Add '.initialize' tests.

--- test/lib/ctrlo/actions/refresh_comments_test.rb
+++ test/lib/ctrlo/actions/refresh_comments_test.rb
@@ -2,5 +2,26 @@ require_relative "../../../test_helper"
 
 module Ctrlo
   describe RefreshComments do
+    let(:described_class) { RefreshComments }
+
+    describe ".initialize" do
+      let(:board_id) { "some_board_id" }
+      let(:options) { {} }
+
+      subject { described_class.new(board_id, options) }
+
+      it "assigns the board_id" do
+        subject.instance_variable_get("@board_id").
+          must_equal("some_board_id")
+      end
+
+      it "assigns the options" do
+        subject.instance_variable_get("@options").
+          must_equal({})
+      end
+    end
+
+    describe "#refresh" do
+    end
   end
 end
