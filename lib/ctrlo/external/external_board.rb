fbb69f66ad18cad84f1cf0e8ba72806be9e3fbd2
Gavin Laking <gavin.laking@onthebeach.co.uk>
2013-12-07 18:59:40 +0000

    Change order of methods, add options attribute.

--- lib/ctrlo/external/external_board.rb
+++ lib/ctrlo/external/external_board.rb
@@ -2,20 +2,18 @@ module Ctrlo
   class ExternalBoard
     include Helpers
 
-    def initialize(external_id = nil)
+    def initialize(external_id = nil, options = {})
       @external_id = external_id
-    end
-
-    def self.fetch_all
-      new.fetch_all
-    end
-
-    def self.fetch_by_external_id(external_id)
-      new(external_id).fetch_by_external_id
+      @options     = options
     end
 
     def self.fetch(external_id, options = {})
       Ctrlo::Board.persist new(external_id, options).fetch_by_external_id
+    end
+
+    def fetch_by_external_id
+      [Trello::Board.find(external_id)]
+    rescue Trello::Error
     end
 
     def self.refresh_all
@@ -24,11 +22,6 @@ module Ctrlo
 
     def fetch_all
       Trello::Board.all
-    rescue Trello::Error
-    end
-
-    def fetch_by_external_id
-      [Trello::Board.find(external_id)]
     rescue Trello::Error
     end
 
