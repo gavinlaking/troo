b980e704c2334095e1a6cd6360fbdbbd9c1a2e05
Gavin Laking <gavinlaking@gmail.com>
2013-12-15 22:17:13 +0000

    Troo::Card now uses Redis.

--- lib/troo/models/card.rb
+++ lib/troo/models/card.rb
@@ -1,35 +1,40 @@
 module Troo
-  class Card
-    include DataMapper::Resource
+  class Card < Ohm::Model
+    include Ohm::DataTypes
     include ModelHelpers
 
-    property :id,                  Serial
-    property :short_id,            Integer
-    property :name,                Text
-    property :desc,                Text
-    #property :due,
-    property :url,                 Text
-    property :external_member_ids, CommaSeparatedList
-    property :position,            Integer
-    property :last_activity_date,  DateTime
-    property :current,             Boolean, default: false
-    property :closed,              Boolean, default: false
-    property :external_board_id,   String
-    property :external_list_id,    String
-    property :external_card_id,    String
+    attribute :short_id
+    attribute :name
+    attribute :desc
+    attribute :url
+    attribute :external_member_ids, Type::Array
+    attribute :position
+    attribute :last_activity_date
+    attribute :current, Type::Boolean
+    attribute :closed, Type::Boolean
+    attribute :external_board_id
+    attribute :external_list_id
+    attribute :external_card_id
 
-    belongs_to :board,    parent_key: [ :external_board_id ],
-                          child_key:  [ :external_board_id ],
-                          required:   true
-    belongs_to :list,     parent_key: [ :external_list_id ],
-                          child_key:  [ :external_list_id ],
-                          required:   true
-    has n,     :comments, parent_key: [ :external_card_id ],
-                          child_key:  [ :external_card_id ],
-                          constraint: :skip
+    index :current
+    index :external_board_id
+    index :external_list_id
+    index :external_card_id
+
+    def board
+      Troo::Board.first(external_board_id: self.external_board_id)
+    end
+
+    def list
+      Troo::List.first(external_list_id: self.external_list_id)
+    end
+
+    def comments
+      Troo::Comment.find(external_card_id: self.external_card_id)
+    end
 
     def members
-      []
+      #Troo::Member.find(external_member_id:) # we need to get all the member ids for the card first
     end
   end
 end
--- test/lib/troo/models/card_test.rb
+++ test/lib/troo/models/card_test.rb
@@ -4,10 +4,108 @@ module Troo
   describe Card do
     let(:described_class) { Card }
 
-    describe "#members" do
-      subject { described_class.new.members }
+    before do
+      Ohm.redis.flushall
+      @card = Troo::Card.create({
+        short_id: "1",
+        name: "My Test Card",
+        desc: "some description",
+        url: "some trello url",
+        external_member_ids: ["5195fdb5a8c01a2318004f5d"],
+        position: 16,
+        last_activity_date: "",
+        current: false,
+        closed: false,
+        external_board_id: "526d8e130a14a9d846001d96",
+        external_list_id: "526d8e130a14a9d846001d97",
+        external_card_id: "526d8f19ddb279532e005259"
+      })
+      @board = Troo::Board.create({
+        external_board_id: "526d8e130a14a9d846001d96"
+      })
+      @list = Troo::List.create({
+        external_list_id: "526d8e130a14a9d846001d97"
+      })
+      @comment = Troo::Comment.create({
+        external_card_id: "526d8f19ddb279532e005259"
+      })
+    end
 
-      it "" do
+    after do
+      @card.delete
+      @board.delete
+      @list.delete
+      @comment.delete
+    end
+
+    it "should have a short_id attribute" do
+      @card.short_id.must_equal "1"
+    end
+
+    it "should have a name attribute" do
+      @card.name.must_equal "My Test Card"
+    end
+
+    it "should have a desc attribute" do
+      @card.desc.must_equal "some description"
+    end
+
+    it "should have a url attribute" do
+      @card.url.must_equal "some trello url"
+    end
+
+    it "should have an external_member_ids attribute" do
+      @card.external_member_ids.must_equal(["5195fdb5a8c01a2318004f5d"])
+    end
+
+    it "should have a position attribute" do
+      @card.position.must_equal 16
+    end
+
+    it "should have a last_activity_date attribute" do
+      @card.last_activity_date.must_equal ""
+    end
+
+    it "should have a current attribute" do
+      @card.current.must_equal false
+    end
+
+    it "should have a closed attribute" do
+      @card.closed.must_equal false
+    end
+
+    it "should have an external_board_id attribute" do
+      @card.external_board_id.must_equal "526d8e130a14a9d846001d96"
+    end
+
+    it "should have an external_list_id attribute" do
+      @card.external_list_id.must_equal "526d8e130a14a9d846001d97"
+    end
+
+    it "should have an external_card_id attribute" do
+      @card.external_card_id.must_equal "526d8f19ddb279532e005259"
+    end
+
+    describe "#board" do
+      it "belongs to a board" do
+        @card.board.must_equal @board
+      end
+    end
+
+    describe "#list" do
+      it "belongs to a list" do
+        @card.list.must_equal @list
+      end
+    end
+
+    describe "#comments" do
+      it "can have many comments" do
+        @card.comments.size.must_equal 1
+      end
+    end
+
+    describe "#members" do
+      it "can have many members" do
         skip
       end
     end
