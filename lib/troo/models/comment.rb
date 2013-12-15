4554f5cdfc148491d690e628b8876194d001bee5
Gavin Laking <gavinlaking@gmail.com>
2013-12-15 22:31:11 +0000

    Troo::Comment now uses Redis.

--- lib/troo/models/comment.rb
+++ lib/troo/models/comment.rb
@@ -1,25 +1,31 @@
 module Troo
-  class Comment
-    include DataMapper::Resource
+  class Comment < Ohm::Model
+    include Ohm::DataTypes
     include ModelHelpers
 
-    property :id,                  Serial
-    property :text,                Text
-    property :date,                DateTime
-    property :external_comment_id, String
-    property :external_board_id,   String
-    property :external_card_id,    String
-    property :external_member_id,  String
+    attribute :text
+    attribute :date
+    attribute :external_board_id
+    attribute :external_card_id
+    attribute :external_comment_id
+    attribute :external_member_id
 
-    belongs_to :board,  parent_key: [ :external_board_id ],
-                        child_key:  [ :external_board_id ],
-                        required:   true
-    belongs_to :card,   parent_key: [ :external_card_id ],
-                        child_key:  [ :external_card_id ],
-                        required:   true
-    belongs_to :member, parent_key: [ :external_member_id ],
-                        child_key:  [ :external_member_id ],
-                        required:   true
+    index :external_board_id
+    index :external_card_id
+    index :external_comment_id
+    index :external_member_id
+
+    def board
+      Troo::Board.first(external_board_id: self.external_board_id)
+    end
+
+    def card
+      Troo::Card.first(external_card_id: self.external_card_id)
+    end
+
+    def member
+      Troo::Member.first(external_member_id: self.external_member_id)
+    end
   end
 end
 
--- test/lib/troo/models/comment_test.rb
+++ test/lib/troo/models/comment_test.rb
@@ -3,6 +3,76 @@ require_relative "../../../test_helper"
 module Troo
   describe Comment do
     let(:described_class) { Comment }
+
+    before do
+      Ohm.redis.flushall
+      @comment = Troo::Comment.create({
+        text: "some comment text",
+        date: "",
+        external_board_id: "526d8e130a14a9d846001d96",
+        external_card_id: "526d8f19ddb279532e005259",
+        external_comment_id: "51f9277b2822b8654f0023af",
+        external_member_id: "5195fdb5a8c01a2318004f5d"
+      })
+      @board = Troo::Board.create({
+        external_board_id: "526d8e130a14a9d846001d96"
+      })
+      @card = Troo::Card.create({
+        external_card_id: "526d8f19ddb279532e005259"
+      })
+      @member = Troo::Member.create({
+        external_member_id: "5195fdb5a8c01a2318004f5d"
+      })
+    end
+
+    after do
+      @comment.delete
+      @board.delete
+      @card.delete
+      @member.delete
+    end
+
+    it "should have a text attribute" do
+      @comment.text.must_equal "some comment text"
+    end
+
+    it "should have a date attribute" do
+      @comment.date.must_equal ""
+    end
+
+    it "should have an external_board_id attribute" do
+      @comment.external_board_id.must_equal "526d8e130a14a9d846001d96"
+    end
+
+    it "should have an external_card_id attribute" do
+      @comment.external_card_id.must_equal "526d8f19ddb279532e005259"
+    end
+
+    it "should have an external_comment_id attribute" do
+      @comment.external_comment_id.must_equal "51f9277b2822b8654f0023af"
+    end
+
+    it "should have a external_member_id attribute" do
+      @comment.external_member_id.must_equal "5195fdb5a8c01a2318004f5d"
+    end
+
+    describe "#board" do
+      it "belongs to a board" do
+        @comment.board.must_equal @board
+      end
+    end
+
+    describe "#card" do
+      it "belongs to a card" do
+        @comment.card.must_equal @card
+      end
+    end
+
+    describe "#member" do
+      it "belongs to a member" do
+        @comment.member.must_equal @member
+      end
+    end
   end
 end
 
