53d6bf5106b149eb026aa1a923afdcf54f53d676
Gavin Laking <gavinlaking@gmail.com>
2013-12-15 16:30:46 +0000

    Remove unused #internal_attributes method.

--- lib/troo/models/model_helpers.rb
+++ lib/troo/models/model_helpers.rb
@@ -1,9 +1,5 @@
 module Troo
   module ModelHelpers
-    def internal_attributes
-      self.attributes.keep_if   { |k, _| k == :id || k == :current }
-    end
-
     def external_attributes
       self.attributes.delete_if { |k, _| k == :id || k == :current }
     end
--- test/lib/troo/models/model_helpers.rb
+++ test/lib/troo/models/model_helpers.rb
@@ -8,14 +8,6 @@ module Troo
   describe ModelHelpersDummy do
     let(:described_class) { ModelHelpersDummy }
 
-    describe "#internal_attributes" do
-      subject { described_class.new.internal_attributes }
-
-      it "" do
-        skip
-      end
-    end
-
     describe "#external_attributes" do
       subject { described_class.new.external_attributes }
 
