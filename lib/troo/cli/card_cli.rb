38afd324160890a9eea764b88e1dde4e1430f45c
Gavin Laking <gavinlaking@gmail.com>
2013-12-21 20:02:20 +0000

    Remove commented out code.

--- lib/troo/cli/card_cli.rb
+++ lib/troo/cli/card_cli.rb
@@ -3,13 +3,11 @@ module Troo
     class Card < Thor
       desc "all <board_id>", "Show all the cards for board <board_id>"
       def all(board_id)
-        # Troo::CLI::Board.new.cards(board_id)
         say "Not implemented yet."
       end
 
       desc "show <card_id>", "Show a card <card_id> (includes comments)"
       def show(card_id)
-        # Troo::Screen.render(Troo::CardDecorator.new(Troo::CardRetrieval.retrieve(card_id)).as_view)
         say "Not implemented yet."
       end
     end
