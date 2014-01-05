57e358193d3c4d270f0744a5580657c93746944b
Gavin Laking <gavinlaking@gmail.com>
2014-01-05 19:41:38 +0000

    Filter OAuth credentials and URLs from cassettes.

--- test/support/vcr_setup.rb
+++ test/support/vcr_setup.rb
@@ -3,5 +3,31 @@ require "vcr"
 VCR.configure do |c|
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
+  c.filter_sensitive_data('<OAuth Credentials>') do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
+
+  # hack hack hack
+  # Couldn't work out how to instruct VCR to filter urls, shortUrls
+  # and shortLinks from the returned body string; hate myself.
+  c.filter_sensitive_data('<Trello URL>') do |interaction|
+    parsed = JSON.parse(interaction.response.body)
+    if parsed.is_a?(Array)
+      parsed.map do |hash|
+        hash.map do |k, v|
+          hash[k] = "<Trello URL>" if (k == "url"      ||
+                                       k == "shortUrl" ||
+                                       k == "shortLink")
+        end
+      end
+      interaction.response.body = JSON.dump(parsed)
+    elsif parsed.is_a?(Hash)
+      parsed.map { |k, v| parsed[k] = "<Trello URL>" if (k == "url"      ||
+                                                         k == "shortUrl" ||
+                                                         k == "shortLink") }
+      interaction.response.body = JSON.dump(parsed)
+    end
+    nil
+  end
 end
 
