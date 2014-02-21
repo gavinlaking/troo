831e6ce3c68ac2dfeb7d4d1f8b8c9b0a0e3bd964
Gavin Laking <gavinlaking@gmail.com>
2014-02-21 20:33:06 +0000

    Remove commented out code.

--- lib/troo/cli/thor_fixes.rb
+++ lib/troo/cli/thor_fixes.rb
@@ -1,10 +1,6 @@
 module Troo
   module CLI
     class ThorFixes < Thor
-      # def self.exit_on_failure?
-      #  true # exit code is non-zero on error
-      # end
-
       def self.banner(command, namespace = nil, subcommand = false)
         if subcommand
           "#{basename} #{@package_name} #{command.usage}"
