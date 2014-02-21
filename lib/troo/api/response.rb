d911c01af84b0fd3a9d7854abb099dec3119a443
Gavin Laking <gavinlaking@gmail.com>
2014-02-21 19:48:16 +0000

    Remove HTTP status code related code;
    Handle errors outside of this class.

--- lib/troo/api/response.rb
+++ lib/troo/api/response.rb
@@ -1,8 +1,6 @@
 module Troo
   module API
     class Response
-      attr_accessor :status, :body
-
       class << self
         def parse(api_response)
           new(api_response).parse
@@ -13,32 +11,14 @@ module Troo
         @api_response = api_response
       end
 
-      def status_code
-        @status_code ||= api_response.code.to_i
-      end
-
       def body
-        return parsed_response  if ok?
-        fail InvalidAccessToken if unauthorized?
-        fail GenericAPIError
+        @parsed ||= Yajl::Parser.parse(api_response.body)
       end
       alias_method :parse, :body
 
       private
 
       attr_reader :api_response
-
-      def parsed_response
-        @body ||= Yajl::Parser.parse(api_response.body)
-      end
-
-      def ok?
-        [200, 201].include?(status_code)
-      end
-
-      def unauthorized?
-        status_code == 401
-      end
     end
   end
 end
