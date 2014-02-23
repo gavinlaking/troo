d9b0a1148ffa54653b05453e2810c1bca6cb9e1d
Gavin Laking <gavinlaking@gmail.com>
2014-02-23 21:43:24 +0000

    Fix spacing issue with subcommands.

--- lib/troo/cli/thor_fixes.rb
+++ lib/troo/cli/thor_fixes.rb
@@ -2,11 +2,7 @@ module Troo
   module CLI
     class ThorFixes < Thor
       def self.banner(command, namespace = nil, subcommand = false)
-        if subcommand
-          "#{basename} #{@package_name} #{command.usage}"
-        else
-          "#{basename}#{@package_name} #{command.usage}"
-        end
+        [basename, @package_name, command.usage].compact.join(" ")
       end
     end
   end
