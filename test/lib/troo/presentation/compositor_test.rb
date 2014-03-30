8ab7054efd8019fe3c83620e45675ca225ffaf4f
Gavin Laking <gavinlaking@gmail.com>
2014-03-30 13:35:56 +0100

    Add tests for Compositor.

--- test/lib/troo/presentation/compositor_test.rb
+++ test/lib/troo/presentation/compositor_test.rb
@@ -6,15 +6,61 @@ module Troo
     let(:described_instance) { described_class.new }
 
     describe '#render' do
-      subject { described_instance.render(lines) }
+      let(:lines) { ["testing\n", "1... 2... 3...\n"] }
+
+      context 'when indentation is set' do
+        before  { @cmp = described_instance }
+
+        subject do
+          capture_io do
+            @cmp.indent { @cmp.render(lines) }
+          end.join
+        end
+
+        it 'renders the contents indented' do
+          subject.must_equal "    testing\n" \
+                             "    1... 2... 3...\n"
+        end
+      end
+
+      context 'when no indentation set' do
+        subject do
+          capture_io do
+            described_instance.render(lines)
+          end.join
+        end
+
+        it 'renders the contents as normal' do
+          subject.must_equal "testing\n" \
+                             "1... 2... 3...\n"
+        end
+      end
     end
 
     describe '#spacer' do
-      subject { described_instance.spacer { } }
-    end
+      context 'when a block is given' do
+        subject do
+          capture_io do
+            described_instance.spacer { 'testing...' }
+          end.join
+        end
 
-    describe '#indent' do
-      subject { described_instance.indent { } }
+        it 'renders the contents of the block between line breaks' do
+          subject.must_equal("\n\n")
+        end
+      end
+
+      context 'when a block is not given' do
+        subject do
+          capture_io do
+            described_instance.spacer
+          end.join
+        end
+
+        it 'renders a single line break' do
+          subject.must_equal("\n")
+        end
+      end
     end
   end
 end
